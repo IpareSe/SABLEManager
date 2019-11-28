96fc53575bc6408254b84e30690b0119cccd9c7d
Paresh Prajapati <paresh.prajapati.sa@gmail.com>
2019-11-28 17:51:28 +0530

    First Commit

---
+++ SABLEManager.framework/Headers/SABLEManager.h
@@ -0,0 +1,19 @@
+//
+//  SABLEManager.h
+//  SABLEManager
+//
+//  Created by Paresh Prajapati on 27/11/19.
+//  Copyright © 2019 Solution Analysts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SABLEManager.
+FOUNDATION_EXPORT double SABLEManagerVersionNumber;
+
+//! Project version string for SABLEManager.
+FOUNDATION_EXPORT const unsigned char SABLEManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SABLEManager/PublicHeader.h>
+
+
